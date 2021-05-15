dbd0f9a9f509c9cb95ad6e6286fd7187dfbc8f60
Kaze Shindo <kaze.bknb@outlook.jp>
2021-05-15 14:59:20 +0900

    django-admin startproject trainkun_apiserver

---
+++ trainkun_apiserver/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for trainkun_apiserver project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainkun_apiserver.settings')
+
+application = get_asgi_application()
---
+++ trainkun_apiserver/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for trainkun_apiserver project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trainkun_apiserver.settings')
+
+application = get_wsgi_application()
