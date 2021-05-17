01a7c9a6c71dac440911773ca02e9966a5585e0e
Kaze Shindo <kaze.bknb@outlook.jp>
2021-05-17 15:36:14 +0900

    override Busstop.API.create

--- busstop/views.py
+++ busstop/views.py
@@ -1,12 +1,21 @@
+import logging
 from django.shortcuts import render
 from django_filters import rest_framework as filters
 from rest_framework import generics
 from .models import Busstop
 from .serializers import BusstopSerializer
 
+logger = logging.getLogger(__name__)
+
 class BusstopCreateAPIView(generics.CreateAPIView):
     """バス停モデルの登録APIクラス"""
     serializer_class = BusstopSerializer
+
+    def create(self, request, *args, **kwargs):
+        """バス停モデルの登録APIに対応するアクションメソッド"""
+        response = super().create(request, *args, **kwargs)
+        logger.info("Busstop(id={})を登録しました".format(response.data['id']))
+        return response
 
 class BusstopListAPIView(generics.ListAPIView):
     """バス停モデルの取得（一覧）APIクラス"""
