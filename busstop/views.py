11e22f0b188973039146664a4e77a30080242537
Kaze Shindo <kaze.bknb@outlook.jp>
2021-05-17 13:30:09 +0900

    create Busstop Views

--- busstop/views.py
+++ busstop/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django_filters import rest_framework as filters
+from rest_framework import generics
+from .models import Busstop
+from .serializers import BusstopSerializer
 
-# Create your views here.
+class BusstopCreateAPIView(generics.CreateAPIView):
+    """バス停モデルの登録APIクラス"""
+    serializer_class = BusstopSerializer
+
+class BusstopListAPIView(generics.ListAPIView):
+    """バス停モデルの取得（一覧）APIクラス"""
+    queryset = Busstop.objects.all()
+    serializer_class = BusstopSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = '__all__'
+
+class BusstopRetrieveAPIView(generics.RetrieveAPIView):
+    """バス停モデルの取得（詳細）APIクラス"""
+    queryset = Busstop.objects.all()
+    serializer_class = BusstopSerializer
+
+class BusstopUpdateAPIView(generics.UpdateAPIView):
+    """バス停モデルの更新・一部更新APIクラス"""
+    queryset = Busstop.objects.all()
+    serializer_class = BusstopSerializer
+
+class BusstopDestroyAPIView(generics.DestroyAPIView):
+    """バス停モデルの削除APIクラス"""
+    queryset = Busstop.objects.all()
