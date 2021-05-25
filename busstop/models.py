0aafa809181ce43105d04907b46734eac0b4bb4d
Kaze Shindo <kaze.bknb@outlook.jp>
2021-05-26 01:00:57 +0900

    uuid to id(int)

--- busstop/models.py
+++ busstop/models.py
@@ -9,7 +9,6 @@ class Busstop(models.Model):
         ordering = ['created_at']
         verbose_name = verbose_name_plural = 'バス停' # rm 's'
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(verbose_name='名称', unique=True, max_length=50)
     code = models.CharField(verbose_name='コード', unique=True, max_length=20)
     created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
