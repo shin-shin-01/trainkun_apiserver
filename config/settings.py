1fb061f86cba865ac5291982ce2f53aaa16a86d4
Kaze Shindo <kaze.bknb@outlook.jp>
2021-05-26 14:36:18 +0900

    change db from sqlite to postgres

--- config/settings.py
+++ config/settings.py
@@ -93,8 +93,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
+        'TEST': {
+            'NAME': 'test_postgres',
+        },
     }
 }
 
