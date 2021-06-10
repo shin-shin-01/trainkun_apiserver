bee71ff033cf3af56ef4fc93ec99a3885169a640
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-10 18:19:50 +0900

    create BusTimetable Model

--- bus_timetable/models.py
+++ bus_timetable/models.py
@@ -1,7 +1,6 @@
 from django.db import models
-from busstop.models import Busstop
-
-# https://stackoverflow.com/questions/60421962/django-change-foreign-keys-to-bigint
+from bus_pair.models import BusPair
+from bus_line.models import BusLine
 
 
 class BusTimetable(models.Model):
@@ -12,20 +11,21 @@ class BusTimetable(models.Model):
         ordering = ['created_at']
         verbose_name = verbose_name_plural = 'バス時刻'  # rm 's'
 
-    # set "db_constraint=False" cuz occurs error
-    # "arrival_bus_stop_id" and "id" are of incompatible types: bigint and uuid.
-    departure_bus_stop = models.ForeignKey(
-        Busstop,
+    bus_pair = models.ForeignKey(
+        BusPair,
         db_constraint=False,
-        verbose_name='出発バス停',
+        verbose_name='バス停組み合わせ',
         on_delete=models.PROTECT,
-        related_name="departure_bus_stop")
-    arrival_bus_stop = models.ForeignKey(
-        Busstop,
+        related_name="bus_pair")
+    bus_line = models.ForeignKey(
+        BusLine,
         db_constraint=False,
-        verbose_name='到着バス停',
+        verbose_name='バス路線',
         on_delete=models.PROTECT,
-        related_name="arrival_bus_stop")
+        related_name="bus_line")
+
+    departure_at = models.DateTimeField(verbose_name='出発日時')
+    arrive_at = models.DateTimeField(verbose_name='到着日時')
     created_at = models.DateTimeField(verbose_name='登録日時', auto_now_add=True)
 
     def __str__(self):
