609a32abf8c92acc058e051d7b5bad0746ff59ae
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-10 19:25:57 +0900

    create TestBusTimetableSerializer

--- bus_timetable/tests/test_serializers.py
+++ bus_timetable/tests/test_serializers.py
@@ -1,12 +1,19 @@
+import pytz
 from django.test import TestCase
 from django.utils.timezone import localtime
 from rest_framework import serializers
 from collections import OrderedDict
+from factory.faker import faker
 
 from ..models import BusTimetable
 from ..serializers import BusTimetableSerializer
-from busstop.factories import BusstopFactory
-from busstop.serializers import BusstopSerializer
+from bus_pair.factories import BusPairFactory
+from bus_pair.serializers import BusPairSerializer
+from bus_line.factories import BusLineFactory
+from bus_line.serializers import BusLineSerializer
+
+FAKE = faker.Faker()
+jst = pytz.timezone('Asia/Tokyo')
 
 
 class TestBusTimetableSerializer(TestCase):
@@ -14,13 +21,17 @@ class TestBusTimetableSerializer(TestCase):
 
     def test_input_valid(self):
         """入力データのバリデーション（OK）"""
-        departure_bus_stop = BusstopFactory()
-        arrival_bus_stop = BusstopFactory()
+        bus_pair = BusPairFactory()
+        bus_line = BusLineFactory()
+        departure_at = FAKE.date_time(tzinfo=jst)
+        arrive_at = FAKE.date_time(tzinfo=jst)
 
         # シリアライザを作成
         input_data = {
-            'departure_bus_stop_id': departure_bus_stop.id,
-            'arrival_bus_stop_id': arrival_bus_stop.id,
+            'bus_pair_id': bus_pair.id,
+            'bus_line_id': bus_line.id,
+            'departure_at': departure_at,
+            'arrive_at': arrive_at,
         }
         serializer = BusTimetableSerializer(data=input_data)
         # バリデーションの結果を検証
@@ -31,24 +42,33 @@ class TestBusTimetableSerializer(TestCase):
 
     def test_output_data(self):
         """出力データの内容検証"""
-        departure_bus_stop = BusstopFactory()
-        arrival_bus_stop = BusstopFactory()
-        departure_bus_stop_serializer = BusstopSerializer(
-            instance=departure_bus_stop)
-        arrival_bus_stop_serializer = BusstopSerializer(
-            instance=arrival_bus_stop)
+        bus_pair = BusPairFactory()
+        bus_line = BusLineFactory()
+        bus_pair_serializer = BusPairSerializer(
+            instance=bus_pair)
+        bus_line_serializer = BusLineSerializer(
+            instance=bus_line)
+
+        departure_at = FAKE.date_time(
+            tzinfo=jst).strftime("%Y-%m-%d %H:%M:%S%z")
+        arrive_at = FAKE.date_time(tzinfo=jst).strftime("%Y-%m-%d %H:%M%:%S%z")
 
         # シリアライザを作成
         bus_timetable = BusTimetable.objects.create(
-            departure_bus_stop=departure_bus_stop,
-            arrival_bus_stop=arrival_bus_stop,
+            bus_pair=bus_pair,
+            bus_line=bus_line,
+            departure_at=departure_at,
+            arrive_at=arrive_at
         )
         serializer = BusTimetableSerializer(instance=bus_timetable)
 
         # シリアライザの出力内容を検証
         expected_data = {
-            'id': bus_timetable.id, 'departure_bus_stop': OrderedDict(
-                departure_bus_stop_serializer.data), 'arrival_bus_stop': OrderedDict(
-                arrival_bus_stop_serializer.data), }
+            'id': bus_timetable.id,
+            'bus_pair': OrderedDict(bus_pair_serializer.data),
+            'bus_line': OrderedDict(bus_line_serializer.data),
+            'departure_at': departure_at,
+            'arrive_at': arrive_at
+        }
 
         self.assertDictEqual(serializer.data, expected_data)
