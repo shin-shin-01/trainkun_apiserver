3b2a7a38581c03931a7b161c5a6d0a67aab5b61c
Kaze Shindo <kaze.bknb@outlook.jp>
2021-06-10 20:24:37 +0900

    set timezone to faker.date_time

--- bus_timetable/factories.py
+++ bus_timetable/factories.py
@@ -1,7 +1,12 @@
 import factory
+from factory.faker import faker
+from datetime import timedelta, timezone
 from .models import BusTimetable
 from bus_pair.factories import BusPairFactory
 from bus_line.factories import BusLineFactory
+
+FAKE = faker.Faker()
+JST = timezone(timedelta(hours=+9), 'JST')
 
 
 class BusTimetableFactory(factory.django.DjangoModelFactory):
@@ -10,5 +15,5 @@ class BusTimetableFactory(factory.django.DjangoModelFactory):
 
     bus_pair = factory.SubFactory(BusPairFactory)
     bus_line = factory.SubFactory(BusLineFactory)
-    departure_at = factory.Faker('date_time')
-    arrive_at = factory.Faker('date_time')
+    departure_at = FAKE.date_time(tzinfo=JST).isoformat()
+    arrive_at = FAKE.date_time(tzinfo=JST).isoformat()
